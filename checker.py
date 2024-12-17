19f1fcafb6542cca44aed1117d978530366ecf7a
civilianrebel <theonlysteve201@gmail.com>
2024-12-17 14:59:47 -0500

    moved checking to a separate func

--- checker.py
+++ checker.py
@@ -1,8 +1,24 @@
 import re
 
 
+def check(password):
+    # returns true if all conditions are met, otherwise returns reason(s)
+    # check if password is at least 8 chars long
+    if len(password) < 8:
+            return False
+    # check if password has an uppercase char
+    # check if password has a lowercase char
+    # check if password has a number
+    # check if password has a special char
+    
+
 if __name__ == '__main__':
     # main
     while True:
-        input('Please input your password. It can be any length and contain any character')
-    # get user input
+        # get user input
+        user_input = input('Please input your password. It can be any length and contain any character')
+        
+
+
+
+        
