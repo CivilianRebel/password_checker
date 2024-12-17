d0cde8f9d7ad88fafd2d13baaf70037627f7c3f6
civilianrebel <theonlysteve201@gmail.com>
2024-12-17 14:52:50 -0500

    first commit

---
+++ checker.py
@@ -0,0 +1,8 @@
+import re
+
+
+if __name__ == '__main__':
+    # main
+    while True:
+        input('Please input your password. It can be any length and contain any character')
+    # get user input
